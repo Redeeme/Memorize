e2af81ac3376248c22c31a9a171e34bf241ea72e
Jozef <Jozefforgac122@gmail.com>
2025-02-12 18:22:44 +0100

    cards and emojis

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,65 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    let smileEmojis: [String] = ["😀", "😃", "😄", "😁", "😆", "😅", "🤣", "😂", "😋", "😎", "😌", "😍", "😘", "😗", "😙", "😚", "😋", "😜", "😝", "😛", "😜", "😒", "😞", "😔", "😕", "😟", "😤",]
+    @State var emojisCount = 5
+    let columns = [GridItem(.adaptive(minimum: 120))]
+    
+    
     var body: some View {
-        VStack {	
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            cards
+            Spacer()
+            countChangers
         }
-        .padding()
+    }
+    
+    var cards: some View{
+        ScrollView{
+            LazyVGrid(columns: columns)
+            {
+                ForEach(0..<emojisCount, id: \.self) { index in
+                    CardView(emoji: smileEmojis[index])
+                        .aspectRatio(2/3,contentMode: .fit)
+                }
+            }.padding(5)
+        }
+    }
+    
+    var countChangers: some View{
+        HStack{
+            countChanger(by: 1, symbol: "plus.diamond",color: .green)
+            Spacer()
+            countChanger(by: -1, symbol: "minus.diamond",color: .red)
+        }.padding(10).font(.largeTitle)
+    }
+    
+    func countChanger(by offset: Int, symbol: String, color:Color)-> some View{
+        Button(action: {
+            emojisCount += offset
+        }, label: {
+            Image(systemName: symbol).tint(color)
+        }).disabled(emojisCount + offset > smileEmojis.count || emojisCount + offset == 0)
     }
 }
 
-struct MyView: View {
+struct CardView: View {
+    let emoji : String
+    @State var isFaceUp: Bool = true
+    
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.brown)
+        ZStack{
+            RoundedRectangle(cornerRadius: 25.0).stroke(lineWidth: 2).fill(Color.blue)
+            Text(emoji)
+            RoundedRectangle(cornerRadius: 25.0).opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
-        .padding()
     }
 }
 
 #Preview {
-    HStack{
-        VStack {
-            HStack{
-                MyView()
-                MyView()
-            }
-            HStack{
-                MyView()
-                MyView()
-            }
-            HStack{
-                MyView()
-                MyView()
-            }
-            HStack{
-                MyView()
-                MyView()
-            }
-        }
-        VStack {
-            HStack{
-                MyView()
-                MyView()
-            }
-            HStack{
-                MyView()
-                MyView()
-            }
-            HStack{
-                MyView()
-                MyView()
-            }
-            HStack{
-                MyView()
-                MyView()
-            }
-        }
-    }
+    ContentView()
 }
